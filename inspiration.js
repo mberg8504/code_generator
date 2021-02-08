73c25d468a33891c5c9d1638d9d103a1f2d0db32
mberg <mbergprogramming@gmail.com>
2021-02-08 03:51:55 -0500

    First commit

---
+++ inspiration.js
@@ -0,0 +1,20 @@
+const quotes = [ "Life is about making an impact, not making an income", 
+                "Whatever the mind of man can conceive and believe, it can achieve", 
+                "Strive not to be a success, but rather to be of value",
+                "Your limitation - it's only your imagination",
+                "Push yourself because no noe else is going to do it for you", 
+                "Great things never come from comfort zones",
+                "Dream bigger, do bigger",
+                "Don't stop when you're tired, stop when you're done",
+                "The more you do today, the closer you'll be to your goals",
+                "It's going to be hard, but hard does not mean impossible"]
+
+
+let randomNum = Math.floor(Math.random() * 11);
+
+const quoteGen = () => {
+    
+}
+
+
+
